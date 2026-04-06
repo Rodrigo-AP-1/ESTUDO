f81dae7d179d5d042d39ec9029f78cc98b90b055
Rodrigoha <vinicius.a.prates2@gmail.com>
2026-04-06 14:34:07 -0300

    arquivo2e3

---
+++ c-brocode/arquivo003/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main() {
+    char grade = 'F';
+    char symbol = '!';
+    char name[] = "Rodrigo";
+
+    printf("A sua nota no exame foi: %c\n", grade);
+    printf("Que vista linda%c\n", symbol);
+    printf("Oi, meu nome eh %s\n", name);
+
+    return 0;
+}
